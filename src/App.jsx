64d50a4dee31cf65b86b2930d9c99292c653225c
Yannick Bley <bleyyannickpro@gmail.com>
2025-01-06 15:51:03 +0100

    feat: add style and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
 
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Home from './pages/Home'
-import About from './pages/About'
+import "./server"
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/about" element={<About />}/>
-          </Routes>
-        </BrowserRouter>
-        
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
   )
-}
+};
 
-export default App
+export default App;
